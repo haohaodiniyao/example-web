679cf858faa24318b15824b1141932ab1ae876a0
yaokai <yaokaiabc@gmail.com>
2017-01-27 23:42:09 +0800

    init

---
+++ src/main/java/com/manyi/iw/uum/TestController.java
@@ -0,0 +1,17 @@
+package com.manyi.iw.uum;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/spring")
+public class TestController {
+	
+	@RequestMapping("/test")
+	@ResponseBody
+	public String msg(String msg){
+		return msg;
+	}
+
+}
