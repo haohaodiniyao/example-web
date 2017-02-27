34eacb6cf7254272518f10c44e9764faff96d713
yaokai <yaokaiabc@gmail.com>
2017-02-27 22:15:57 +0800

    mybatis c3po

--- src/test/java/com/example/test/JunitTest.java
+++ src/test/java/com/example/test/JunitTest.java
@@ -1,35 +1,38 @@
 package com.example.test;
 
-import java.util.Iterator;
-import java.util.Set;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.example.base.server.bus.BusServer;
-import com.google.common.collect.Sets;
+import com.example.base.mapper.User;
+import com.example.base.mapper.UserMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:spring/spring-context.xml")
+@ContextConfiguration(locations = "classpath:spring/*.xml")
 public class JunitTest {
 //	@Autowired
 //	private BusServer busServer;
+	
+	@Autowired
+	private UserMapper userMapper;
 	
 	@Test
 	public void test(){
 //		Method method = BeanUtils.findMethod(TestBean.class, "getResult",String.class);
 //		busServer.call("hello world!");
 		
-		Set<String> set = Sets.newHashSet();
-		set.add("1");
-		set.add("2");
-		Iterator<String> list = set.iterator();
-		while(list.hasNext()){
-			System.out.println(list.next());
-		}
+//		Set<String> set = Sets.newHashSet();
+//		set.add("1");
+//		set.add("2");
+//		Iterator<String> list = set.iterator();
+//		while(list.hasNext()){
+//			System.out.println(list.next());
+//		}
+		User user = new User();
+		user.setCode1(new Byte("-128"));
+		userMapper.insertSelective(user);
 	}
 	
 }
