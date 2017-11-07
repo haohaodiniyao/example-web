ea2a373e2cdf92c4a8256fad0ce8e3c151079c72
yaokai <yaokaiabc@gmail.com>
2017-11-07 13:12:16 +0800

    update

--- src/main/test/com/example/base/test/JunitTest.java
+++ src/main/test/com/example/base/test/JunitTest.java
@@ -1,57 +1,37 @@
 package com.example.base.test;
 
-import java.util.Date;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.base.mapper.User;
-import com.example.base.server.user.UserService;
+import com.example.base.mapper.UserMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)  
 @ContextConfiguration(locations={"classpath:root.xml"})
 public class JunitTest {
 	@Autowired
-	private UserService userService;
-    @Autowired  
-    private ApplicationContext applicationContext;  
+	private UserMapper userMapper;
 	@Test
 	public void test() {
-//		for(int i=0;i<100;i++){
-//			int index = i+1;
-//	        new Thread(new Runnable(){
-//				@Override
-//				public void run() {
-//					RegisterEvent registerEvent = new RegisterEvent();
-//					registerEvent.setMsg("hello world#"+index);
-//					applicationContext.publishEvent(registerEvent);
-//				}
-//	        }).start();			
-//		}
-//
-//
-//        try {
-//			Thread.sleep(5000*1000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-		User record = new User();
-		record.setUserId(101);
-		record.setUserName("hello123");
-		record.setDeptName("dept123");
-		record.setCreateDate(new Date());
-		try {
-			userService.saveUser(record);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(record);
 		
+		User user = new User();
+//		user.setId(1L);
+//		user.setUid(100);
+//		userMapper.updateByPrimaryKeySelective(user);
+//		String type = "2";
+//		switch(type){
+//			case "1":
+//				System.out.println(1);
+//			case "2":
+//				System.out.println(2);
+//			case "3":
+//				System.out.println(3);
+//			default:
+//				System.out.println(4);	
+//		}
 	}
 	
 }
